2bfc34c6de42321bcca9f886e3e19a104e04428a
rebeccaflack <68822249+rebeccaflack@users.noreply.github.com>
2021-01-17 18:19:30 -0500

    Upload to Git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="container">
+      <br />
+      <div className="card app-body">
+        <div className="card-body">
+          <form id="search-form">
+            <input
+              type="text"
+              placeholder="Where in the world..."
+              id="search-text-input"
+            />
+            <input type="submit" value="🔍" id="search" />
+            <input
+              type="submit"
+              value="📍 Current"
+              id="current-location-button"
+            />
+          </form>
+          <br />
+          <h1 className="city">Toronto</h1>
+          <h5>Last updated:</h5>
+          <h5>
+            <span id="current-date"></span>
+          </h5>
+          <h1 className="current-temperature">
+            <span id="current-temperature"> 24 </span>
+            <span className="units">
+              <a href="#" id="celsius-link" className="active">
+                ˚C
+              </a>{" "}
+              |
+              <a href="#" id="farenheit-link">
+                ˚F
+              </a>
+            </span>
+          </h1>
+          <div className="row">
+            <div className="col">
+              <p>
+                <span id="weather-description"> Partly cloudy </span>
+                <br />
+                Wind: <span id="wind"> 11 </span> km/h
+                <br />
+                Humidity: <span id="humidity">90</span>%
+              </p>
+            </div>
+            <div className="col center-block text-center">
+              <img
+                src="http://openweathermap.org/img/wn/10d@2x.png"
+                className="current-weather-icon"
+                id="icon"
+                alt="weather icon"
+              />
+            </div>
+          </div>
+          <div className="card text-center">
+            <div className="card-body">
+              <p className="quote">
+                "The Earth has music for those who listen"
+                <br />- George Santayana
+              </p>
+            </div>
+          </div>
+        </div>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="https://github.com/rebeccaflack/GIT-SheCodesPlusProject"
+          target="blank"
+          className="link"
         >
-          Learn React
+          Open-source code by Rebecca Flack
         </a>
-      </header>
+        <br />
+      </div>
+
+      <br />
+      <br />
     </div>
   );
 }
-
-export default App;
